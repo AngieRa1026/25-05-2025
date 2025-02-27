ab54ed3589cffffc7cecdeeb318ac83d31d6a625
Angie Ramirez <angieramirez1026@gmail.com>
2025-02-27 08:54:58 -0500

    Mensaje

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa los grados celsius");
+        byte celsius = scanner.nextByte();
+        scanner.close();
+        float fahrenheit = (byte) (celsius * 9/5 + 32);
+
+        System.out.println(celsius + " grados celsius son " + fahrenheit + " grados fahrenheit");
     }
 }
